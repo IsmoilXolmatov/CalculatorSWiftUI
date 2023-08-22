f0047534f99de15dd4c809ba53c43a22d63fac09
IosDevelopUZ <126049891+IosDevelopUZ@users.noreply.github.com>
2023-08-22 17:09:54 +0500

    Initial Commit

---
+++ SwiftUIAnimation1SideMenu/SwiftUIAnimation1SideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimation1SideMenuApp.swift
+//  SwiftUIAnimation1SideMenu
+//
+//  Created by Khalmatov on 22.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimation1SideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
